df9feea2b9d26adcb196a3a8be1c7f619ac68a8b
lucifer <xhw562193521@outlook.com>
2022-12-11 22:39:58 +0800

    create app

--- day01/videomanager/videomanager/settings.py
+++ day01/videomanager/videomanager/settings.py
@@ -47,6 +47,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    # add app
+    # demo1
+    # 'book'
+    # demo2
+    'video.apps.VideoConfig'
 ]
 
 ROOT_URLCONF = 'videomanager.urls'
